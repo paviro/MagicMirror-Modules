fb79e2511c13a8098a7c756d51ddccb2e1526dae
Paul-Vincent Roll <paviro@me.com>
2016-03-31 02:27:25 +0200

    removed code

--- bin/SocketIOServer/addressbook.js
+++
@@ -1,4 +0,0 @@
-obj = {};
-obj["put phone number here"] = "name of caller";
-obj["another phone number"] = "other name";
-
--- www/modules/paviro/include.php
+++
@@ -1,9 +0,0 @@
-<?php
-return array(
-	'js_files' => array(
-		"http://localhost:1234/socket.io/socket.io.js"
-	),
-	'css_files' => array(
-	)
-);
-?>
